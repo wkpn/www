e8b94bee38b50278ecf010b2b4c825e18b817279
wkpn <yegornosov@gmail.com>
2020-02-13 00:45:35 +0300

    prettify

--- app.py
+++ app.py
@@ -4,51 +4,45 @@ from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
-
-app = Starlette()
-
-app.mount('/css', StaticFiles(directory='css'), name='css')
-app.mount('/images', StaticFiles(directory='images'), name='images')
-app.mount('/js', StaticFiles(directory='js'), name='js')
-app.mount('/icon', StaticFiles(directory='icon'), name='icon')
-
-templates = Jinja2Templates(directory='templates')
-
-index_data = {
-    'title': 'why\'',
-    'name': 'Egor Nosov',
-    'position': 'Python Developer at Grid Dynamics',
-    'image': '/images/avatar.jpg'
-}
-
-error_page_data = {
-    'title': 'Page not found',
-    'error_message': 'Page \'{url}\' could not be found'
-}
+from data import index_data, error_page_data
 
 
-@app.route('/', methods=['GET'])
-async def index(request: Request):
-    """
-        Basic index page with social links and brief information
-    """
-    template = 'index.html'
-    context = {'request': request}
-    context.update(index_data)
+def build_application():
+    app = Starlette()
 
-    return templates.TemplateResponse(template, context)
+    app.mount('/css', StaticFiles(directory='css'), name='css')
+    app.mount('/images', StaticFiles(directory='images'), name='images')
+    app.mount('/js', StaticFiles(directory='js'), name='js')
+    app.mount('/icon', StaticFiles(directory='icon'), name='icon')
+
+    templates = Jinja2Templates(directory='templates')
+
+    @app.route('/', methods=['GET'])
+    async def index(request: Request):
+        """
+            Basic index page with social links and brief information
+        """
+        template = 'index.html'
+        context = {'request': request}
+        context.update(index_data)
+
+        return templates.TemplateResponse(template, context)
+
+    @app.exception_handler(exc_class_or_status_code=404)
+    async def not_found(request: Request, exc: HTTPException):
+        """
+            Friendly 404 page with a rocket animation
+        """
+        template = '404.html'
+        context = {
+            'request': request,
+            'request_url': request.url.path
+        }
+        context.update(error_page_data)
+
+        return templates.TemplateResponse(template, context, status_code=404)
+
+    return app
 
 
-@app.exception_handler(exc_class_or_status_code=404)
-async def not_found(request: Request, exc: HTTPException):
-    """
-        Friendly 404 page with a rocket animation
-    """
-    template = '404.html'
-    context = {
-        'request': request,
-        'request_url': request.url.path
-    }
-    context.update(error_page_data)
-
-    return templates.TemplateResponse(template, context, status_code=404)
+app = build_application()
---
+++ data.py
@@ -0,0 +1,11 @@
+index_data = {
+    'title': 'why\'',
+    'name': 'Egor Nosov',
+    'position': 'Python Developer at Grid Dynamics',
+    'image': '/images/avatar.jpg'
+}
+
+error_page_data = {
+    'title': 'Page not found',
+    'error_message': 'Page \'{url}\' could not be found'
+}
