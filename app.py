842ef6cc66e38e6aea0fa660a1b492ea4be9765e
wkpn <yegornosov@gmail.com>
2020-06-07 20:26:41 +0300

    lower level, typing annotations

--- app.py
+++ app.py
@@ -1,44 +1,47 @@
-from starlette.applications import Starlette
-from starlette.exceptions import HTTPException
 from starlette.requests import Request
+from starlette.routing import Mount, Route, Router
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from data import index_data, error_page_data
 
 
-def build_application():
-    app = Starlette()
+def build_application() -> Router:
+    templates = Jinja2Templates(directory="templates")
 
-    for directory in ('css', 'images', 'js', 'icon'):
-        app.mount(f'/{directory}', StaticFiles(directory=directory), name=directory)
-
-    templates = Jinja2Templates(directory='templates')
-
-    @app.route('/', methods=['GET'])
-    async def index(request: Request):
+    async def index(request: Request) -> templates.TemplateResponse:
         """
-            Basic index page with social links and brief information
+        Basic index page with social links and brief information
         """
-        template = 'index.html'
-        context = {'request': request}
-        context.update(index_data)
+        template = "index.html"
+        context = {
+            "request": request,
+            **index_data
+        }
 
         return templates.TemplateResponse(name=template, context=context)
 
-    @app.exception_handler(exc_class_or_status_code=404)
-    async def not_found(request: Request, exc: HTTPException):
+    async def error(request: Request) -> templates.TemplateResponse:
         """
-            Friendly 404 page with a rocket animation
+        Friendly 404 page with a rocket animation
         """
-        template = '404.html'
+        template = "404.html"
         context = {
-            'request': request,
-            'request_url': request.url.path
+            "request": request,
+            "request_url": request.url.path,
+            **error_page_data
         }
-        context.update(error_page_data)
 
         return templates.TemplateResponse(name=template, context=context, status_code=404)
+
+    app = Router(routes=[
+        Route("/", index, methods=["GET"]),
+        Route("/{path}", error, methods=["GET"]),
+        Mount("/css", StaticFiles(directory="css"), name="css"),
+        Mount("/images", StaticFiles(directory="images"), name="images"),
+        Mount("/js", StaticFiles(directory="js"), name="js"),
+        Mount("/icon", StaticFiles(directory="icon"), name="icon")
+    ])
 
     return app
 
